65b103077df54c664bebf3f7b5ef574eea4c7347
vjosullivan <vjosullivan@hotmail.com>
2019-09-26 14:25:46 +0100

    Initial commit.
    Vanilla Xcode project.

---
+++ MazesTests/MazesTests.swift
@@ -0,0 +1,34 @@
+//
+//  MazesTests.swift
+//  MazesTests
+//
+//  Created by Vincent O'Sullivan on 26/09/2019.
+//  Copyright © 2019 Vincent O'Sullivan. All rights reserved.
+//
+
+import XCTest
+@testable import Mazes
+
+class MazesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
